336e5ed7771c610d304a8f35b217e041e1ef367b
Júnior Silva <devtucuju@gmail.com>
2022-08-26 11:24:02 -0300

    adiciona componente de input

--- src/screens/User/Login/index.js
+++ src/screens/User/Login/index.js
@@ -1,11 +1,52 @@
 import React from "react";
-import {View,Text} from "react-native";
-import {Container} from "./styles"
+import {View,Text, StyleSheet} from "react-native";
+import Responsive from 'react-native-lightweight-responsive';
+import { Input } from "../../../components/Forms/Input";
+
+import theme from "../../../globals/styles/theme"
 
 export function Login (){
 return (
-    <Container>
-        <Text>Login Screen</Text>
-    </Container>
+    <View style={styles.container}>
+        <Text style={styles.title}>Entrar</Text>
+        <View style={styles.form}>
+        <Text style={styles.inputTitle}>Email</Text>
+     <Input
+     placeholder="exemple@email.com"
+     />
+      <Text style={styles.inputTitle}>Password</Text>
+      <Input
+   
+     />
+     </View>
+    </View>
 )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+justifyContent :"center",
+alignItems: "center",
+
+
+    },
+title:{
+    fontSize:Responsive.font(26),
+    color:theme.colors.title,
+ 
+
+},
+inputTitle:{
+    fontSize:Responsive.font(14),
+    color:theme.colors.primary
+    ,
+},
+form:{
+    flex: 1,
+    width: "100%",
+    justifyContent :"center",
+    padding: Responsive.font(24)
+}
+}
+)
